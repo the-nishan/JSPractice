296ec351ae7e7b2d262348d9caaf8b22dc35bb9d
Didaur Rahman <nishan.profile@gmail.com>
2025-09-28 16:23:53 +0600

    new js editir opened for pop push shift unshift

---
+++ push pop shift unshift.js
@@ -0,0 +1,13 @@
+let persons = [];
+let addedPersons =persons.push("Alice", "Bob", "Charlie");
+console.log(persons);
+
+let newArrival =persons.unshift("zarra");
+console.log(persons);
+
+let theFirstPerson =persons.shift();
+console.log(theFirstPerson);
+console.log(persons);
+
+let leftPerson=persons.pop();
+console.log(persons);
