a2f19f58d96f044b22706ef2616facacc61c889c
Didaur Rahman <nishan.profile@gmail.com>
2025-10-01 11:32:00 +0600

    problem solved

--- CompareTwoString.js
+++ CompareTwoString.js
@@ -1,7 +1,18 @@
-var compareTwoString = (str1,str2)=>{
-    if(str1 === str2)
-        return true
-    return false
-}
+// var compareTwoString = (str1,str2)=>{
+//     if(str1 === str2)
+//         return true
+//     return false
+// }
 
-console.log(compareTwoString('joe','bob'))
+// console.log(compareTwoString('joe','bob'))
+
+
+
+compare =(a,b) => {
+    if(a===b) {
+        return "True";
+    } else {
+        return "False";
+    }
+}
+console.log(compare("book","pen"));
