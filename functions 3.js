e35007f9155ec202e6607a0f34f5118609221c30
Didaur Rahman <nishan.profile@gmail.com>
2025-10-04 09:06:37 +0600

    object add||delete||for IN

---
+++ functions 3.js
@@ -0,0 +1,18 @@
+// let car ={
+//     brand: "toyota",
+//     model: "x2",
+//     year: 1999,
+// }
+
+// car.condition = "New";
+// delete car.model;
+
+
+// console.log(car);
+
+
+let vegetabls= {name:"potato", taste:"good", fat:"yes"};
+
+for(let key in vegetabls){
+    console.log(key+ ":"+ vegetabls[key]);
+}
