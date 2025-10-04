1450cad382126f1c1fe4e9479fbaa163145c964e
Didaur Rahman <nishan.profile@gmail.com>
2025-10-04 15:40:52 +0600

    example 2

--- reduce.js
+++ reduce.js
@@ -1,7 +1,28 @@
-let arr = [4, 989, 3, 765, 9984, 293, 656, 239];
+/* let arr = [4, 989, 3, 765, 9984, 293, 656, 239];
 
 max = (accumulator, element) => {
   return Math.max(accumulator, element);
 };
 let maximum = arr.reduce(max);
-console.log(maximum);
+console.log(maximum); */
+
+// example 1= sum of array elements
+
+/*  let arr1 = [1, 2, 3, 4, 5];
+let sum = (accu, ele) => {
+    return (accu + ele);
+}
+
+let summation = arr1.reduce(sum);
+console.log(summation); */
+
+//example 2 = product of array
+
+let arr2 = [2, 7, 0.000001];
+
+funcProduct = (acc, ele) => {
+  return acc * ele;
+};
+
+let getProduct = arr2.reduce(funcProduct);
+console.log(getProduct);
