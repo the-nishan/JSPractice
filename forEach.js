9a401cfbfcb161f202cbe2e0c8bca0eb4f0ea5e7
Didaur Rahman <nishan.profile@gmail.com>
2025-10-05 17:03:48 +0600

    forEach example

--- forEach.js
+++ forEach.js
@@ -1,7 +1,7 @@
 let list = ["shirt", "pant", "panjabi", "perfume", "shoe", "smartwatch"];
 let newList = [];
 
-  list.forEach(printItems => {
-newList.push(printItems);
-})
+list.forEach((printItems) => {
+  newList.push(printItems);
+});
 console.log(newList);
