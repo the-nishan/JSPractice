96caf98158cc4e72322bd2dac7bdafe6c772056f
Didaur Rahman <nishan.profile@gmail.com>
2025-10-06 10:04:10 +0600

    for each by params

--- forEach.js
+++ forEach.js
@@ -1,7 +1,17 @@
-let list = ["shirt", "pant", "panjabi", "perfume", "shoe", "smartwatch"];
+/* let list = ["shirt", "pant", "panjabi", "perfume", "shoe", "smartwatch"];
 let newList = [];
 
 list.forEach((printItems) => {
   newList.push(printItems);
 });
-console.log(newList);
+console.log(newList); */
+
+
+
+let list = ["shirt", "pant", "panjabi", "perfume", "shoe", "smartwatch"];
+// let newList = [];
+
+list.forEach((item, index) => {
+  // newList.push(item, index);
+  console.log(item, index);
+});
