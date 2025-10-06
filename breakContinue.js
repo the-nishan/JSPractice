94aab77125bc21cfeed54e6a829e907eeb6d49dd
Tunvir Rahman <tusher@nsales.dk>
2025-10-06 11:21:59 +0600

    break continue sample code

---
+++ breakContinue.js
@@ -0,0 +1,39 @@
+
+// Break Code example
+
+
+// var sampleCode = (str)=>{
+
+//     var finalString = ""
+//     for(var i = 0;i<str.length;i++){ // loop body
+//         if(str[i] === 'x' || str[i] === "X"){
+//             break;
+//         }
+//         finalString = finalString + str[i];
+//     }//end of the loop
+//     return finalString;
+// }
+
+
+//console.log(sampleCode("Boxer"))
+
+
+
+// continue code example
+
+
+var sampleCode = (str)=>{
+
+    var finalString = ""
+    for(var i = 0;i<str.length;i++){ // loop body
+        if(str[i] === 'x' || str[i] === "X"){
+            continue;
+        }
+        finalString = finalString + str[i];
+    }//end of the loop
+    return finalString;
+}
+
+
+console.log(sampleCode("Boxer"))
+
