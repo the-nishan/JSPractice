33a44e9e3fcde0543f41d8416944a3929e4723af
Didaur Rahman <nishan.profile@gmail.com>
2025-10-08 13:22:54 +0600

    practice

--- practiceJS.js
+++ practiceJS.js
@@ -1,14 +1,14 @@
 //reverse
-let revString = (word) => {
+/* let revString = (word) => {
   return word
   .split("")
   .reverse()
   .join("");
 };
-console.log(revString("redmi"));
+console.log(revString("redmi")); */
 
 //panlindrome
-let palindromeCheck = (word) => {
+/* let palindromeCheck = (word) => {
   let revWord = [];
   word
   .split("")
@@ -17,4 +17,38 @@ let palindromeCheck = (word) => {
   revWord.push(word);
   return revWord;
 };
-console.log(palindromeCheck("abc"));
+console.log(palindromeCheck("abc")); */
+
+//find vowels
+/* let funcCountVowels = (word) => {
+  let vowels = [];
+for(let i=0;i<word.length;i++) {
+if(word[i] === "a" || word[i] === "e" || word[i] === "i" ||
+  word[i] === "o" || word[i] === "u") {
+    vowels.push(word[i]);
+  }
+}
+
+
+  return(vowels);
+}
+console.log(funcCountVowels("paper")); */
+
+//count vowels
+let funcCountVowels = (word) => {
+  let vowelCount = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (
+      word[i] === "a" ||
+      word[i] === "e" ||
+      word[i] === "i" ||
+      word[i] === "o" ||
+      word[i] === "u"
+    ) {
+      vowelCount++;
+    }
+  }
+
+  return vowelCount;
+};
+console.log(funcCountVowels("paper"));
