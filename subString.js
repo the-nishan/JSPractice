dd01c78f33c8398622f64e82b33acde248eb21d0
Tunvir Rahman <tusher@nsales.dk>
2025-10-15 11:22:04 +0600

    skeleton problem

---
+++ subString.js
@@ -0,0 +1,11 @@
+var isSubString = (mainString, subString) =>{
+
+
+
+}
+
+console.log(isSubString("amar sonar bangla.","bangla") // Should return true
+console.log(isSubString("amar sonar bangla","Abngladesh") // Should return false
+console.log(isSubString("amar sonar bangla","abc") // Should return false
+console.log(isSubString("amar sonar bangla","sonar") // Should return true
+console.log(isSubString("Bangladesh","Bangla") // Should return true
