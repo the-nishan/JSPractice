2cb6f4beff470437c4ff7983bebcffd6f02e2906
Didaur Rahman <>
2025-11-03 17:27:43 +0600

    JSONParsingPractice. solved

--- JSONParsingPractice.js
+++ JSONParsingPractice.js
@@ -1,81 +1,101 @@
 const jsonObject = [
   {
-    "name": "Aminul Islam",
-    "national_id": "1990234567891",
-    "age": 34,
-    "gender": "Male",
-    "address": "Mohakhali DOHS, Dhaka"
+    name: "Aminul Islam",
+    national_id: "1990234567891",
+    age: 34,
+    gender: "Male",
+    address: "Mohakhali DOHS, Dhaka",
   },
   {
-    "name": "Farzana Haque",
-    "national_id": "1987456123098",
-    "age": 29,
-    "gender": "Female",
-    "address": "Banani, Dhaka"
+    name: "Farzana Haque",
+    national_id: "1987456123098",
+    age: 29,
+    gender: "Female",
+    address: "Banani, Dhaka",
   },
   {
-    "name": "Rafiul Karim",
-    "national_id": "2001345678123",
-    "age": 25,
-    "gender": "Male",
-    "address": "Agrabad, Chittagong"
+    name: "Rafiul Karim",
+    national_id: "2001345678123",
+    age: 25,
+    gender: "Male",
+    address: "Agrabad, Chittagong",
   },
   {
-    "name": "Taslima Akter",
-    "national_id": "1998765432109",
-    "age": 31,
-    "gender": "Female",
-    "address": "Uposhohor, Sylhet"
+    name: "Taslima Akter",
+    national_id: "1998765432109",
+    age: 31,
+    gender: "Female",
+    address: "Uposhohor, Sylhet",
   },
   {
-    "name": "Mahmudul Hasan",
-    "national_id": "1995678901234",
-    "age": 40,
-    "gender": "Male",
-    "address": "Kushtia Sadar, Khulna"
+    name: "Mahmudul Hasan",
+    national_id: "1995678901234",
+    age: 40,
+    gender: "Male",
+    address: "Kushtia Sadar, Khulna",
   },
   {
-    "name": "Nusrat Jahan",
-    "national_id": "2000456123789",
-    "age": 27,
-    "gender": "Female",
-    "address": "Rajshahi City, Rajshahi"
+    name: "Nusrat Jahan",
+    national_id: "2000456123789",
+    age: 27,
+    gender: "Female",
+    address: "Rajshahi City, Rajshahi",
   },
   {
-    "name": "Sakib Al Mamun",
-    "national_id": "1989345678012",
-    "age": 36,
-    "gender": "Male",
-    "address": "Uttara Sector 7, Dhaka"
+    name: "Sakib Al Mamun",
+    national_id: "1989345678012",
+    age: 36,
+    gender: "Male",
+    address: "Uttara Sector 7, Dhaka",
   },
   {
-    "name": "Rumana Yasmin",
-    "national_id": "1996789054321",
-    "age": 33,
-    "gender": "Female",
-    "address": "Barishal Town, Barishal"
+    name: "Rumana Yasmin",
+    national_id: "1996789054321",
+    age: 33,
+    gender: "Female",
+    address: "Barishal Town, Barishal",
   },
   {
-    "name": "Fahim Reza",
-    "national_id": "2002123456789",
-    "age": 22,
-    "gender": "Male",
-    "address": "Narsingdi, Dhaka Division"
+    name: "Fahim Reza",
+    national_id: "2002123456789",
+    age: 22,
+    gender: "Male",
+    address: "Narsingdi, Dhaka Division",
   },
   {
-    "name": "Sadia Rahman",
-    "national_id": "1994567890123",
-    "age": 30,
-    "gender": "Female",
-    "address": "Bogra Sadar, Rajshahi"
-  }
-]
+    name: "Sadia Rahman",
+    national_id: "1994567890123",
+    age: 30,
+    gender: "Female",
+    address: "Bogra Sadar, Rajshahi",
+  },
+];
 
+let malesName = jsonObject
+  .filter((info) => info.gender === "Male")
+  .map((info) => info.name);
+let femalesName = jsonObject
+  .filter((info) => info.gender === "Female")
+  .map((info) => info.name);
+let ageBetn = jsonObject
+  .filter((info) => info.age >= 30 && info.age <= 40)
+  .map((info) => info.name);
 
+//
+let livingBarisal = jsonObject
+  .filter((info) => info.address.includes("Barishal"))
+  .map((info) => info.name);
+//
+let maleDhaka = jsonObject
+  .filter((info) => info.address.includes("Dhaka") && info.gender === "Male")
+  .map((info) => info.name);
+//
 
-console.log(jsonObject)
-
-
+console.log(malesName);
+console.log(femalesName);
+console.log(ageBetn);
+console.log(livingBarisal);
+console.log(maleDhaka);
 
 // Find all name of male
 // find all name of female
