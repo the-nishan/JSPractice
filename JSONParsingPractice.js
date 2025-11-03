7e7ce43459d09feb1a0f7d93ea852edf9333ad93
Tunvir Rahman <tusher@nsales.dk>
2025-11-03 11:12:35 +0600

    basic JSON

---
+++ JSONParsingPractice.js
@@ -0,0 +1,84 @@
+const jsonObject = [
+  {
+    "name": "Aminul Islam",
+    "national_id": "1990234567891",
+    "age": 34,
+    "gender": "Male",
+    "address": "Mohakhali DOHS, Dhaka"
+  },
+  {
+    "name": "Farzana Haque",
+    "national_id": "1987456123098",
+    "age": 29,
+    "gender": "Female",
+    "address": "Banani, Dhaka"
+  },
+  {
+    "name": "Rafiul Karim",
+    "national_id": "2001345678123",
+    "age": 25,
+    "gender": "Male",
+    "address": "Agrabad, Chittagong"
+  },
+  {
+    "name": "Taslima Akter",
+    "national_id": "1998765432109",
+    "age": 31,
+    "gender": "Female",
+    "address": "Uposhohor, Sylhet"
+  },
+  {
+    "name": "Mahmudul Hasan",
+    "national_id": "1995678901234",
+    "age": 40,
+    "gender": "Male",
+    "address": "Kushtia Sadar, Khulna"
+  },
+  {
+    "name": "Nusrat Jahan",
+    "national_id": "2000456123789",
+    "age": 27,
+    "gender": "Female",
+    "address": "Rajshahi City, Rajshahi"
+  },
+  {
+    "name": "Sakib Al Mamun",
+    "national_id": "1989345678012",
+    "age": 36,
+    "gender": "Male",
+    "address": "Uttara Sector 7, Dhaka"
+  },
+  {
+    "name": "Rumana Yasmin",
+    "national_id": "1996789054321",
+    "age": 33,
+    "gender": "Female",
+    "address": "Barishal Town, Barishal"
+  },
+  {
+    "name": "Fahim Reza",
+    "national_id": "2002123456789",
+    "age": 22,
+    "gender": "Male",
+    "address": "Narsingdi, Dhaka Division"
+  },
+  {
+    "name": "Sadia Rahman",
+    "national_id": "1994567890123",
+    "age": 30,
+    "gender": "Female",
+    "address": "Bogra Sadar, Rajshahi"
+  }
+]
+
+
+
+console.log(jsonObject)
+
+
+
+// Find all name of male
+// find all name of female
+// find all name of age between 30-40
+// find all people living in Barishal
+// Find people who is male and live in Dhaka
